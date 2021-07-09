51e586e639abe740346e14e31953cec2709957a7
TrueTurtleMan <84719452+TrueTurtleMan@users.noreply.github.com>
2021-07-09 18:38:41 +0200

    Fix bugs du menu options
    - Régler le problème du plein écran
    - Découverte de plusieurs bugs (concernant la résolution) que j'ai réglé aujourd'hui
    - Sauvegarde désormais les préférences du joueur pour la résolution et l'option plein écran (même si on ferme le jeu)

--- Assets/Scripts/test_TurtleMan/OptionsMenu.cs
+++ Assets/Scripts/test_TurtleMan/OptionsMenu.cs
@@ -7,7 +7,8 @@ public class OptionsMenu : MonoBehaviour
 {
 
     Resolution[] resolutions;
-    public Dropdown resolutionDropdown; 
+    public Dropdown resolutionDropdown;
+    public Toggle FullscreenToggle; 
 
     void Start()
     {
@@ -15,21 +16,27 @@ public class OptionsMenu : MonoBehaviour
         List<string> listResolution = new List<string>();
         int currentResolutionIndex = 0;
 
+
         resolutions = Screen.resolutions;  // on liste les résolutions possibles pour l'écran utilisé
         resolutionDropdown.ClearOptions(); // on efface les options du dropdown
 
         for (int i = 0; i < resolutions.Length; i++) //on crée une liste composée de résolutions (sous format string) et on cherche quelle résolution est utilisée pour enregistrer l'indice correspondant
         {
-            listResolution.Add(resolutions[i].width + " x " + resolutions[i].height);
-            if (resolutions[i].height == Screen.currentResolution.height && resolutions[i].width == Screen.currentResolution.width)
+            listResolution.Add(resolutions[i].ToString());
+            if (resolutions[i].height == Screen.currentResolution.height && resolutions[i].width == Screen.currentResolution.width && !(PlayerPrefs.HasKey("resolutionIndex")))
             {
                 currentResolutionIndex = i; 
             }
         }
         
         resolutionDropdown.AddOptions(listResolution); //On affiche les différentes options possibles sur le dropdown
-        resolutionDropdown.value = currentResolutionIndex; //La valeur affichée est celle positionnée à l'indice précédemment trouvé  
+        if (!PlayerPrefs.HasKey("resolutionIndex")) resolutionDropdown.value = currentResolutionIndex; //La valeur affichée est celle positionnée à l'indice précédemment trouvé  
+        else resolutionDropdown.value = PlayerPrefs.GetInt("resolutionIndex"); //La valeur affichée est celle que le joueur à choisi précédemment 
         resolutionDropdown.RefreshShownValue(); //On refresh
+
+
+        //Gestion du toggleFullscreen_____________________________________________________________________________________________________________________
+        FullscreenToggle.isOn = Screen.fullScreen; 
     }
 
     public void setResolution(int resolutionIndex)
@@ -37,6 +44,7 @@ public class OptionsMenu : MonoBehaviour
         resolutions = Screen.resolutions;   // on liste les résolutions possibles pour l'écran utilisé
         Resolution resolution = resolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);  //On choisit la résolution selon l'indice sélectionné par le joueur dans le dropdown (sauf si option fullscreen cochée)
+        PlayerPrefs.SetInt("resolutionIndex", resolutionIndex); //sauvegarde l'indice choisi de resolutionDropdown
     }
 
     public void setFullScreen(bool isFullScreen)    //On affichera le jeu en plein écran si l'option "fullscreen" est cochée
